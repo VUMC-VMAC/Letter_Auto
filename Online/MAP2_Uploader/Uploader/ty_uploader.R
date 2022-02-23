c9ad1215247109522bb2c42ab14dbe7cf3f6095c
U-VANDERBILT\sweelyb <sweelyb@NLMACW--FQQXRZ2.dhcp.mc.vanderbilt.edu>
2022-02-23 16:46:57 -0600

    ty letter update

--- Online/MAP2_Uploader/Uploader/ty_uploader.R
+++ Online/MAP2_Uploader/Uploader/ty_uploader.R
@@ -29,8 +29,6 @@ ty_uploader <- function(epoch,vmac) {
   pdb_datas <- exportReports(pdb, 267464)
   cond_datas <- exportReports(EDC,284613)
   
-  path_in <- paste0(main_path,"resources/Templates/Thank You/Thank You Template.docx")
-  
   events <- c("eligibility_arm_1","enrollmentbaseline_arm_1","18month_followup_arm_1","3year_followup_arm_1","5year_followup_arm_1","7year_followup_arm_1")
   pdb_datas <- pdb_datas[which(pdb_datas[,"redcap_event_name"]== events[epoch+1]),]
   ii <- which(pdb_datas["vmac_id"]==as.integer(vmac)) #need to find i for map id
@@ -51,44 +49,68 @@ ty_uploader <- function(epoch,vmac) {
   cmr <- cond_data$cmr_complete; if (is.na(cmr)) {cmr <- "No"}
   ipc <- cond_data$int_ptp_complete; if (is.na(ipc)) {ipc <- "No"}
   
+  i <- 1
+  e <- epoch
+  map_id <- as.character(pdb_data[i,"map_id"])
+  if (nchar(map_id)==1) {input <- paste0("00",map_id)} else if (nchar(map_id)==2) {input <- paste0("0",map_id)} else {input <- map_id}
+  #if (nchar(map_id)==1) {input <- paste0("00",map_id)} else if (nchar(map_id)==2) {input <- paste0("0",map_id)} else {input <- map_id}
+  vmac_id <- as.character(pdb_data[i,"vmac_id"])
+  if (nchar(vmac_id)==1) {record <- paste0("0000",vmac_id)} else if (nchar(vmac_id)==2) {record <- paste0("000",vmac_id)} else if (nchar(vmac_id)==3) {record <- paste0("00",vmac_id)} else if (nchar(vmac_id)==4) {record <- paste0("0",vmac_id)} else {record <- vmac_id}
+  last_name <<- pdb_data[i, "last_name"]
+  salutation <<- as.character(pdb_data[i,"salutation"])
+  
+  epoch_conv <- c("enrollment","18 month","3 year","5 year","7 year","9 year","11 year","13 year")
+  epoch <<- epoch_conv[e]
+  epoch_next <<- epoch_conv[e+1]
+  ep_n_conv <- c("","3yr_","5yr_","7yr_","9yr_","11yr_","13yr_")
+  ep_conv <- c("elig","enroll","18mos","3yr","5yr","7yr","9yr","11yr","13yr")
+  ep_next <- ep_n_conv[e]
+  ep <- ep_conv[e+1]
+  
   #if (length(visit_sched)==0) {vs <- "No"} else {vs <- visit_sched}
-  if (nc != "Yes" & ec != "Yes" & bc != "Yes" & brc != "Yes" & cmr != "Yes" & ipc != "Yes") {err <<- "Insufficient data for thank you letter."} else {
-    err <<- ""
-    
-    i <- 1
-    e <- epoch
-    map_id <- as.character(pdb_data[i,"map_id"])
-    if (nchar(map_id)==1) {input <- paste0("00",map_id)} else if (nchar(map_id)==2) {input <- paste0("0",map_id)} else {input <- map_id}
-    #if (nchar(map_id)==1) {input <- paste0("00",map_id)} else if (nchar(map_id)==2) {input <- paste0("0",map_id)} else {input <- map_id}
-    vmac_id <- as.character(pdb_data[i,"vmac_id"])
-    if (nchar(vmac_id)==1) {record <- paste0("0000",vmac_id)} else if (nchar(vmac_id)==2) {record <- paste0("000",vmac_id)} else if (nchar(vmac_id)==3) {record <- paste0("00",vmac_id)} else if (nchar(vmac_id)==4) {record <- paste0("0",vmac_id)} else {record <- vmac_id}
-    last_name <<- pdb_data[i, "last_name"]
-    salutation <<- as.character(pdb_data[i,"salutation"])
-    
-    epoch_conv <- c("enrollment","18 month","3 year","5 year","7 year","9 year","11 year","13 year")
-    epoch <<- epoch_conv[e]
-    epoch_next <<- epoch_conv[e+1]
-    ep_n_conv <- c("","3yr_","5yr_","7yr_","9yr_","11yr_","13yr_")
-    ep_conv <- c("elig","enroll","18mos","3yr","5yr","7yr","9yr","11yr","13yr")
-    ep_next <- ep_n_conv[e]
-    ep <- ep_conv[e+1]
-    
-    if (e==0){
-      el <-  redcapConnection(url = "https://redcap.vanderbilt.edu/api/",
-                              token = "BFEF4C303E170DDAA850D56C4FB9594A", conn, project = 134345)
-      el_data <- exportReports(el, 271784)
+  if (e > 0) {
+    if (nc != "Yes" & ec != "Yes" & bc != "Yes" & brc != "Yes" & cmr != "Yes" & ipc != "Yes") {err <<- "Insufficient data for thank you letter."} else {
+      err <<- ""
       
-      ii <- which(el_data["vmac_id"]==as.integer(vmac)) #need to find i for map id
-      
-      elig_out <- el_data[ii,"elig_outcome"]
-      enroll_stat <- el_data[ii,"enrollment_status"]
-      
-      path_in <- paste0(main_path,"resources/Templates/Thank You/Thank You Template_e.docx")
-    } else {
       path_in <- paste0(main_path,"resources/Templates/Thank You/Thank You Template.docx")
       
-      fu <<- "To date, your participation has helped generate more than 130 scientific publications and conference presentations, and it has provided significant teaching opportunities for more than 40 investigators and clinicians-in-training."
+      if (e > 1) {
+        fu <<- "To date, your participation has helped generate more than 130 scientific publications and conference presentations, and it has provided significant teaching opportunities for more than 40 investigators and clinicians-in-training."
+      } else {fu <<- ""}
+      
+      date_next <- paste0("visit_estimate_",ep_next,"date")
+      date_ty <<- format(as.Date(pdb_data[i, date_next]), "%B %Y")
+      
+      output <- paste0(out_path,"MAP_",input,"_",ep,"_ty_letter.docx")
+      renderInlineCode(path_in, output)
+      
+      importFiles(rcon = pdb, file = output, record = record, field = "card_thank_letter", event = pdb_data[,"redcap_event_name"],
+                  overwrite = TRUE, repeat_instance = 1)
     }
+  }
+  
+  if (e==0){
+    err <- ""
+    
+    el <-  redcapConnection(url = "https://redcap.vanderbilt.edu/api/",
+                            token = "BFEF4C303E170DDAA850D56C4FB9594A", conn, project = 134345)
+    el_data <- exportReports(el, 271784)
+    
+    ii <- which(el_data["vmac_id"]==as.integer(vmac)) #need to find i for map id
+    
+    elig_out <- el_data[ii,"elig_outcome"]; if (is.na(elig_out)) {elig_out <- "Yes"}
+    enroll_stat <- el_data[ii,"enroll_visit_sched"]; if (is.na(enroll_stat)) {enroll_stat <- "No"}
+    
+    if (elig_out == "No") {
+      elig <<- "Even though it was determined that you are not eligible to participate in the Vanderbilt Memory & Aging Project, we appreciate you donating your time to our research efforts. Thank you for your contribution to this important project. Our efforts would not be possible without your generous help and time."
+    } else {
+      if (enroll_stat == "No") {
+        elig <<- "You are now part of a valuable research study that will help middle aged and older adults at risk for memory loss. Your participation will help answer important questions about how heart health relates to brain health. We will be in touch with you by phone to schedule your enrollment visit. Thank you for your contribution to this important project. Our efforts would not be possible without your generous help."
+      } else {
+        elig <<- "You are now part of a valuable research study that will help middle aged and older adults at risk for memory loss. Your participation will help answer important questions about how heart health relates to brain health. We look forward to seeing you at your upcoming enrollment visit on `enroll_date`. Thank you for your contribution to this important project. Our efforts would not be possible without your generous help."
+      }
+    }
+    path_in <- paste0(main_path,"resources/Templates/Thank You/Thank You Template_e.docx")
     
     date_next <- paste0("visit_estimate_",ep_next,"date")
     date_ty <<- format(as.Date(pdb_data[i, date_next]), "%B %Y")
