8fe8bfb93725c28b7d1a626c1c9bcbed5cacfc32
U-VANDERBILT\sweelyb <sweelyb@NLMACW--FQQXRZ2.dhcp.mc.vanderbilt.edu>
2022-04-28 10:16:02 -0500

    MAP fb update

--- Online/MAP2_Uploader/Uploader/fb_uploader.R
+++ Online/MAP2_Uploader/Uploader/fb_uploader.R
@@ -28,34 +28,6 @@ fb_uploader<<- function(epochh,vmac) {
     
   }
   
-  library(ggplot2)
-  library(plotly)
-  library(redcapAPI)
-  library(WordR)
-  library(officer)
-  library(Hmisc)
-  library(tidyverse)
-  library(flextable)
-  #library(ggpattern)
-  #library(patternplot)
-  library(png)
-  library(readxl)
-  
-  # Global Pathing
-  local <- 1
-  online <- 0
-  if (local) {
-    # Add Local Paths Here
-    out_path <- "C:/Users/sweelyb/Documents/output/"
-    main_path <- "C:/Users/sweelyb/Documents/Letter_Auto/"
-    
-  } else if (online) {
-    # Add Global Paths Here
-    out_path <- "/app/"
-    main_path <- "/srv/shiny-server/"
-    
-  }
-  
   ex_path <- paste0(main_path,"epoch5dde_lookup.xlsx")
   
   FII <- redcapConnection(url = "https://redcap.vanderbilt.edu/api/",
